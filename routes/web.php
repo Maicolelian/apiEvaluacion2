1b36fc61f6363a8b7c3a25ae03c666131dba6dbb
Maicol Elian <elianmuepaz29@gmail.com>
2024-02-06 08:53:05 -0500

    actualizacion de ruta

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UsuariosController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 Route::get('/home', [App\Http\Controllers\UsuariosController::class, 'index'])->name('home');
